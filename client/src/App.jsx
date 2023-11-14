e56fd6e7a8bab28a1bbbb4f24530f0a61245c58d
ajummer <makeudreamempty@gmail.com>
2023-11-14 11:40:50 +0200

    Add catalog and create components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
-import Header from "./components/Header/Header.jsx"
-import Home from "./components/Home/Home.jsx"
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header/Header.jsx";
+import Home from "./components/Home/Home.jsx";
+import Catalog from "./components/Catalog/Catalog.jsx";
+import Create from "./components/Create/Create.jsx";
 function App() {
   return (
     <div id="box">
-    <Header/>
-    <Home/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/games" element={<Catalog/>}></Route>
+        <Route path="/create" element={<Create/>}></Route>
+      </Routes>
     </div>
-  )
-  
-  
+  );
 }
-export default App
+export default App;
