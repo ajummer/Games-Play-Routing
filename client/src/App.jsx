95cb0510dd4cd51d95aeb22ab6f9a74af6eecca3
ajummer <makeudreamempty@gmail.com>
2023-11-14 11:47:14 +0200

    Add self closing tag to route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,14 +4,18 @@ import Header from "./components/Header/Header.jsx";
 import Home from "./components/Home/Home.jsx";
 import Catalog from "./components/Catalog/Catalog.jsx";
 import Create from "./components/Create/Create.jsx";
+import Login from "./components/Login/Login.jsx";
+import Register from "./components/Register/Register.jsx";
 function App() {
   return (
     <div id="box">
       <Header />
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/games" element={<Catalog/>}></Route>
-        <Route path="/create" element={<Create/>}></Route>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/games" element={<Catalog/>}/>
+        <Route path="/create" element={<Create/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
       </Routes>
     </div>
   );
