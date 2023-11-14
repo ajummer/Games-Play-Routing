493213090d8b1650572cca155a8c87ad2eb7741b
ajummer <makeudreamempty@gmail.com>
2023-11-14 13:36:51 +0200

    Add dynamic info to catalog

--- client/src/components/Catalog/Catalog.jsx
+++ client/src/components/Catalog/Catalog.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import * as gameService from "../../services/gameService.js";
+import SingleGame from "../SingleGame/SingleGame.jsx";
 export default function Catalog() {
   const [games, setGames] = useState([]);
 
@@ -11,38 +12,12 @@ export default function Catalog() {
     <section id="catalog-page">
       <h1>All Games</h1>
       {/* Display div: with information about every game (if any) */}
-      <div className="allGames">
-        <div className="allGames-info">
-          <img src="./images/avatar-1.jpg" />
-          <h6>Action</h6>
-          <h2>Cover Fire</h2>
-          <a href="#" className="details-button">
-            Details
-          </a>
-        </div>
-      </div>
-      <div className="allGames">
-        <div className="allGames-info">
-          <img src="./images/avatar-1.jpg" />
-          <h6>Action</h6>
-          <h2>Zombie lang</h2>
-          <a href="#" className="details-button">
-            Details
-          </a>
-        </div>
-      </div>
-      <div className="allGames">
-        <div className="allGames-info">
-          <img src="./images/avatar-1.jpg" />
-          <h6>Action</h6>
-          <h2>MineCraft</h2>
-          <a href="#" className="details-button">
-            Details
-          </a>
-        </div>
-      </div>
-      {/* Display paragraph: If there is no games  */}
-      <h3 className="no-articles">No articles yet</h3>
+    {games.map(game => (
+      <SingleGame game={game} key={game._id}/>
+    ))}
+  
+      {games.length === 0 && <h3 className="no-articles">No articles yet</h3>}
+    
     </section>
   );
 }
